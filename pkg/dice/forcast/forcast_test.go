7ca3ac0c9960bded327a229d1965ba09557d7dee
Galdoba <galdoba@mail.ru>
2024-06-17 14:37:11 +1000

    message text

--- pkg/dice/forcast/forcast_test.go
+++ pkg/dice/forcast/forcast_test.go
@@ -1,7 +1,22 @@
 package forcast
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestForcast(t *testing.T) {
-	TenDiceOucomes(60466176)
+	// f, _ := os.OpenFile("/home/galdoba/go/src/github.com/Galdoba/tabletoptools/pkg/dice/forcast/maps.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
+	// f.Truncate(0)
+	// defer f.Close()
+	// for d := 1; d <= 10; d++ {
+	// 	f.Write([]byte(fmt.Sprintf("dice%vMap := make(map[int][]int)\n", d)))
+	// 	for i := 1 * d; i <= 6*d; i++ {
+	// 		l, e, m := diceOucomes(d, i)
+	// 		fmt.Printf("dice%vMap[%v] = []int{%v, %v, %v}\n", d, i, l, e, m)
+
+	// 		f.Write([]byte(fmt.Sprintf("dice%vMap[%v] = []int{%v, %v, %v}\n", d, i, l, e, m)))
+	// 	}
+	// }
+	fmt.Println(T5_Equal_Or_More(14, 3))
 }
