4c1c351861b1e62160488b02752720f159f14d34
Galdoba <galdoba@mail.ru>
2024-11-02 18:38:06 +1000

    starSystem

--- pkg/mgt2/structs/starsystem/starsystem.go
+++ pkg/mgt2/structs/starsystem/starsystem.go
@@ -25,49 +25,156 @@ type Star struct {
 	Eccentricity float64
 }
 
-var starClass = "(Ia|Ib|II|III|IV|V|VI|BD|D)"
-var starType = "(O|B|A|F|G|K|M|L|T|Y|)"
+var starClass = "(Ia|Ib|II|III|IV|V|VI|BD|D)?"
+var starType = "(O|B|A|F|G|K|M|L|T|Y|)?"
 var starTSTconnector = "( )?"
-var starSubType = "([0123456789])"
-var typeFloat = `(-[0123456789](\.)?([0123456789])?([0123456789])?([0123456789])?([0123456789])?([0123456789])?([0123456789])?)?`
-var starAge = `([0123456789](\.)?([0123456789])?([0123456789])?([0123456789])?([0123456789])?([0123456789])?([0123456789])?)?`
-var starDesignation = `((:)?(A|Aa|Ab|Aab|B|Ba|Bb|Bab|C|Ca|Cb|Cab|D|Da|Db|Dab|))?`
+var starSubType = "([0123456789])?"
+var typeFloat = `(-[0123456789]([0123456789])?([0123456789])?(\.)?([0123456789])?([0123456789])?([0123456789])?([0123456789])?([0123456789])?([0123456789])?)?`
+var starDesignation = `((:)?(Aab|Aa|Ab|A|Bab|Ba|Bb|B|Cab|Ca|Cb|C|Dab|Da|Db|D|))?`
 
 var starProfile = fmt.Sprintf(`%v%v%v%v%v%v%v%v%v%v%v`, starType, starSubType, starTSTconnector, starClass, typeFloat, typeFloat, typeFloat, typeFloat, starDesignation, typeFloat, typeFloat)
 
+var starNumber = `([2345678]-)?` + starProfile
+
+var starSystem = fmt.Sprintf(`%v%v`, starNumber, starProfile)
+
 var anyEhex = "([0123456789ABCDEFGHJKLMNPQRSTUVWXYZ])"
 
-func FromProfile(s string) (*Star, error) {
+func (st *Star) Profile() string {
+	s := ""
+	s += fmt.Sprintf("%v", st.Type)
+	s += fmt.Sprintf("%v", st.SubType)
+	if st.SubType != "" {
+		s += " "
+	}
+	s += fmt.Sprintf("%v", st.Class)
+	if st.Mass > 0.0 {
+		s += fmt.Sprintf("-%v", strconv.FormatFloat(st.Mass, 'f', -1, 64))
+	}
+	if st.Diameter > 0.0 {
+		s += fmt.Sprintf("-%v", strconv.FormatFloat(st.Diameter, 'f', -1, 64))
+	}
+	if st.Luminosity > 0.0 {
+		s += fmt.Sprintf("-%v", strconv.FormatFloat(st.Luminosity, 'f', -1, 64))
+	}
+	if st.Age > 0.0 {
+		s += fmt.Sprintf("-%v", strconv.FormatFloat(st.Age, 'f', -1, 64))
+	}
+	if st.Designation != "" {
+		s += fmt.Sprintf(":%v", st.Designation)
+	}
+	if st.OrbitN > 0.0 {
+		s += fmt.Sprintf("-%v", strconv.FormatFloat(st.OrbitN, 'f', -1, 64))
+	}
+	if st.Eccentricity > 0.0 {
+		s += fmt.Sprintf("-%v", strconv.FormatFloat(st.Eccentricity, 'f', -1, 64))
+	}
 
-	return nil, nil
+	return s
 }
 
-func parseStar(s string) (*Star, error) {
+func (ss *StarSystem) Profile() string {
+	s := ""
+	switch len(ss.Stars) {
+	case 1:
+		s = fmt.Sprintf("%v", ss.Stars[0].Profile())
+	default:
+		age := ss.Age
+		for i := range ss.Stars {
+			ss.Stars[i].Age = 0
+		}
+		ss.Stars[0].Age = age
+		s += fmt.Sprintf("%v", len(ss.Stars))
+		for _, star := range ss.Stars {
+			s += fmt.Sprintf("-%v", star.Profile())
+		}
+	}
+	return s
+}
+
+func FromProfile(from string) (*StarSystem, error) {
+	ss := StarSystem{}
+	// mask := starSystem
+	// re := regexp.MustCompile(mask)
+	// parsed := re.FindString(from)
+	num, next := grepStarNumber(from)
+	switch num {
+	case 1:
+		st, err := StarFromProfile(next)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse star: %v", err)
+		}
+		ss.Stars = append(ss.Stars, st)
+	default:
+		rest := strings.TrimPrefix(from, fmt.Sprintf("%v-", num))
+		for i := 0; i < num; i++ {
+			reString := starProfile
+			re := regexp.MustCompile(reString)
+			next := re.FindString(rest)
+			st, err := StarFromProfile(next)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse star %v: %v\n%v", num, err, rest)
+			}
+			ss.Stars = append(ss.Stars, st)
+			rest = strings.TrimPrefix(rest, next)
+			rest = strings.TrimPrefix(rest, "-")
+
+		}
+	}
+	ss.Age = ss.Stars[0].Age
+	for i := range ss.Stars {
+		if i == 0 {
+			continue
+		}
+		ss.Stars[i].Age = 0
+	}
+	return &ss, nil
+}
+
+func grepStarNumber(s string) (int, string) {
+	for i := 0; i < 9; i++ {
+		pref := fmt.Sprintf("%v-", i)
+		//fmt.Println(pref)
+		if strings.HasPrefix(s, pref) {
+			return i, strings.TrimPrefix(s, pref)
+		}
+	}
+	return 1, s
+}
+
+func StarFromProfile(s string) (*Star, error) {
 	reString := starProfile
 	re := regexp.MustCompile(reString)
 	parsed := re.FindString(s)
 	if parsed == "" {
-		return nil, fmt.Errorf("00failed to parse starsystem from '%v'", s)
+		return nil, fmt.Errorf("00failed to parse stars from '%v'", s)
 	}
 	st := Star{}
 	for i, mask := range []string{starType, starSubType, starTSTconnector, starClass, typeFloat, typeFloat, typeFloat, typeFloat, starDesignation, typeFloat, typeFloat} {
 		switch mask {
 		case starTSTconnector:
 			_, parsed = parseString(parsed, mask)
+			//			fmt.Println("case starTSTconnector:", parsed)
 		case starType:
 			st.Type, parsed = parseString(parsed, mask)
+			//			fmt.Println("case starType:", st.Type)
 		case starSubType:
 			st.SubType, parsed = parseString(parsed, mask)
+			//			fmt.Println("case starSubType:", st.SubType)
 		case starClass:
 			st.Class, parsed = parseString(parsed, mask)
+			//			fmt.Println("case Class:", st.Class)
 		case starDesignation:
 			st.Designation, parsed = parseString(parsed, mask)
+			//			fmt.Println("case starDesignation:", st.Designation)
 		case typeFloat:
 			float, rest, err := parseFloat(parsed)
 			if err != nil {
-				return nil, fmt.Errorf("failed to parse starsystem from '%v': detected %v (%v)", parsed, float, rest)
+				continue
+				//return nil, fmt.Errorf("failed to parse star from '%v': detected %v (%v)", parsed, float, rest)
 			}
 			parsed = rest
+			//			fmt.Println("case float:", float, err)
 			switch i {
 			case 4:
 				st.Mass = float
@@ -84,42 +191,6 @@ func parseStar(s string) (*Star, error) {
 			}
 		}
 	}
-
-	// st.Type, parsed = parseString(parsed, starType)
-	// st.SubType, parsed = parseString(parsed, starSubType)
-	// _, parsed = parseString(parsed, starTSTconnector)
-	// st.Class, parsed = parseString(parsed, starClass)
-	// for i := 0; i < 7; i++ {
-	// 	if parsed == "" {
-	// 		continue
-	// 	}
-	// 	switch i {
-	// 	case 4:
-	// 		st.Designation, parsed = parseString(parsed, starDesignation)
-	// 	default:
-	// 		float, rest, err := parseFloat(parsed)
-	// 		if err != nil {
-	// 			return nil, fmt.Errorf("failed to parse starsystem from '%v': detected %v (%v)", parsed, float, rest)
-	// 		}
-	// 		parsed = rest
-	// 		switch i {
-	// 		case 0:
-	// 			st.Mass = float
-	// 		case 1:
-	// 			st.Diameter = float
-	// 		case 2:
-	// 			st.Luminosity = float
-	// 		case 3:
-	// 			st.Age = float
-	// 		case 5:
-	// 			st.OrbitN = float
-	// 		case 6:
-	// 			st.Eccentricity = float
-	// 		}
-	// 	}
-
-	// 	//parsed = strings.TrimPrefix(parsed, parsedFloat)
-	// }
 
 	return &st, nil
 }
--- pkg/mgt2/structs/starsystem/starsystem_test.go
+++ pkg/mgt2/structs/starsystem/starsystem_test.go
@@ -1,6 +1,7 @@
 package starsystem
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -72,8 +73,62 @@ func Test_parseStar(t *testing.T) {
 		}, // TODO: Add test cases.
 		{
 			name: "2",
-			args: args{"G7 V-0.929-0.967-0.738-6.336"},
+			args: args{"G8 V-0.907-0.957-0.681:B-6.1-0.08"},
 			want: &Star{
+				Type:         "G",
+				SubType:      "8",
+				Class:        "V",
+				Mass:         0.907,
+				Diameter:     0.957,
+				Luminosity:   0.681,
+				Age:          0,
+				Designation:  "B",
+				OrbitN:       6.1,
+				Eccentricity: 0.08,
+			},
+			wantErr: false,
+		}, // TODO: Add test cases.
+		{
+			name: "3",
+			args: args{"D-0.490-0.017-0.000525"},
+			want: &Star{
+				Type:         "",
+				SubType:      "",
+				Class:        "D",
+				Mass:         0.49,
+				Diameter:     0.017,
+				Luminosity:   0.000525,
+				Age:          0,
+				Designation:  "",
+				OrbitN:       0,
+				Eccentricity: 0,
+			},
+			wantErr: false,
+		}, // TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StarFromProfile(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseStar() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseStar() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStar_Profile(t *testing.T) {
+	tests := []struct {
+		name string
+		st   *Star
+		want string
+	}{
+		{
+			name: "",
+			st: &Star{
 				Type:         "G",
 				SubType:      "7",
 				Class:        "V",
@@ -85,18 +140,112 @@ func Test_parseStar(t *testing.T) {
 				OrbitN:       0,
 				Eccentricity: 0,
 			},
+			want: "G7 V-0.929-0.967-0.738-6.336",
+		}, // TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.st.Profile(); got != tt.want {
+				t.Errorf("Star.Profile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFromProfile(t *testing.T) {
+	type args struct {
+		from string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *StarSystem
+		wantErr bool
+	}{
+		{
+			name: "----",
+			args: args{"5-G7 V-0.929-0.967-0.738-6.336:Ab-0.09-0.11-G8 V-0.907-0.957-0.681:B-6.1-0.08-K8 V-0.626-0.777-0.136:Ca-12.1-0.47-M0 V-0.510-0.728-0.0895:Cb-0.21-0.24-D-0.490-0.017-0.000525"},
+			want: &StarSystem{
+				Stars: []*Star{
+					{
+						Type:         "G",
+						SubType:      "7",
+						Class:        "V",
+						Mass:         0.929,
+						Diameter:     0.967,
+						Luminosity:   0.738,
+						Age:          6.336,
+						Designation:  "Ab",
+						OrbitN:       0.09,
+						Eccentricity: 0.11,
+					},
+					{
+						Type:         "G",
+						SubType:      "8",
+						Class:        "V",
+						Mass:         0.907,
+						Diameter:     0.957,
+						Luminosity:   0.681,
+						Age:          0,
+						Designation:  "B",
+						OrbitN:       6.1,
+						Eccentricity: 0.08,
+					},
+					{
+						Type:         "K",
+						SubType:      "8",
+						Class:        "V",
+						Mass:         0.626,
+						Diameter:     0.777,
+						Luminosity:   0.136,
+						Age:          0,
+						Designation:  "Ca",
+						OrbitN:       12.1,
+						Eccentricity: 0.47,
+					},
+					{
+						Type:         "M",
+						SubType:      "0",
+						Class:        "V",
+						Mass:         0.510,
+						Diameter:     0.728,
+						Luminosity:   0.0895,
+						Age:          0,
+						Designation:  "Cb",
+						OrbitN:       0.21,
+						Eccentricity: 0.24,
+					},
+					{
+						Type:         "",
+						SubType:      "",
+						Class:        "D",
+						Mass:         0.49,
+						Diameter:     0.017,
+						Luminosity:   0.000525,
+						Age:          0,
+						Designation:  "",
+						OrbitN:       0,
+						Eccentricity: 0,
+					},
+				},
+				Age: 6.336,
+			},
 			wantErr: false,
 		}, // TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := parseStar(tt.args.s)
+			got, err := FromProfile(tt.args.from)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("parseStar() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("FromProfile() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("parseStar() = %v, want %v", got, tt.want)
+				for i, _ := range got.Stars {
+					fmt.Println(tt.want.Stars[i].Profile())
+					fmt.Println(got.Stars[i].Profile())
+				}
+				t.Errorf("FromProfile() = %v, want %v", got, tt.want)
 			}
 		})
 	}
